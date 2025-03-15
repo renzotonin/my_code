8d9b933b97e2cfc09c8a5f1ac9a4bdfac5b8348c
renzotonin <65802303+renzotonin@users.noreply.github.com>
2025-03-15 15:45:21 +1100

    Add files via upload

---
+++ app_1_map.py
@@ -0,0 +1,22 @@
+from shiny import reactive
+from shiny.express import input, ui
+from shinywidgets import render_widget
+import ipyleaflet as ipyl
+
+city_centers = {
+    "Sydney": (-33.8651,151.2099),
+    "London": (51.5074, 0.1278),
+    "Paris": (48.8566, 2.3522),
+    "New York": (40.7128, -74.0060),
+}
+ui.input_select("center", "Center", choices=list(city_centers.keys()))
+
+
+@render_widget
+def map():
+    return ipyl.Map(zoom=8)
+
+
+@reactive.effect
+def _():
+    map.widget.center = city_centers[input.center()]
